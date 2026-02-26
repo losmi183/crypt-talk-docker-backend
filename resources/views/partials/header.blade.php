090c7e3d78e1a3ff81b32e27d0a3423df51c4d55
milos.glogovac <milos.glogovac@gmail.com>
2026-02-26 13:41:57 +0100

    frontpage navigation fixed

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,15 +1,15 @@
-    <!-- Navigation -->
-    <nav>
-        <div class="container">
-            <div class="logo">
-                🔐 Crypt Talk
-            </div>
-            <ul class="nav-links">
-                <li><a href="#features">Features</a></li>
-                <li><a href="/how-it-works.blade">How It Works</a></li>
-                <li><a href="/platforms">Platforms</a></li>
-                <li><a href="/pricing">Pricing</a></li>
-                <li><a href="/get-started" class="cta-nav">Get Started</a></li>
-            </ul>
+<!-- Navigation -->
+<nav>
+    <div class="container">
+        <div class="logo">
+            🔐 Crypt Talk
         </div>
-    </nav>
+        <ul class="nav-links">
+            <li><a href="#features">Features</a></li>
+            <li><a href="#how-it-works">How It Works</a></li>
+            <li><a href="#platforms">Platforms</a></li>
+            <li><a href="#pricing">Pricing</a></li>
+            <li><a href="https://web.crypt-talk.online/register" class="cta-nav">Get Started</a></li>
+        </ul>
+    </div>
+</nav>
